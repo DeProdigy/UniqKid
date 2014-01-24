915e3cd71209ebf204e1b18553825f4dce64c291
DeProdigy <alex.hint.ah@gmail.com>
2014-01-24 16:36:19 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UniqKid::Application.config.secret_token = '2a9bfb88d89f0ea01372de3755d23c30117ac88df61226438a315f2c7dcad9be0cdbe511d38427379b6063a003269d3ce0d6bbd7c57b4dc2f6fcb86f0d056db6'

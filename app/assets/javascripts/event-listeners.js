e898be9bfcabc272857d6f57aea77b9e6923a154
DeProdigy <alex.hint.ah@gmail.com>
2014-03-09 15:23:35 -0400

    chmpaxify works but only on double submit

--- app/assets/javascripts/event-listeners.js
+++ app/assets/javascripts/event-listeners.js
@@ -1,13 +1,11 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   console.log( "Loaded Main Page" );
 
   $('#signup-form').on('submit', function(event){
     event.preventDefault();
+    console.log('form submited');
 
-    $(function() {
-      $('#signup-form').chimpaxify({'url': 'http://uniqkid.us3.list-manage.com/subscribe/post?u=f4f4089d6992b16f24b9e6640&amp;id=8d386d8bbc'});
-      console.log('submited');
-    });
-
+    $('#signup-form').chimpaxify({'url': 'http://uniqkid.us3.list-manage.com/subscribe/post?u=f4f4089d6992b16f24b9e6640&amp;id=8d386d8bbc'});
+    console.log('sent to mail chimp');
   });
 });

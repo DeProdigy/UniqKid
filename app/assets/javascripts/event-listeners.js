f620e1b51cb0ae165eea51b8a00384f6cedbb015
DeProdigy <alex.hint.ah@gmail.com>
2014-03-09 22:46:40 -0400

    chimpaxify issue solved

--- app/assets/javascripts/event-listeners.js
+++ app/assets/javascripts/event-listeners.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
   console.log( "Loaded Main Page" );
 
-  $('#signup-form').on('submit', function(event){
-    event.preventDefault();
-    console.log('form submited');
+  $('#signup-form').chimpaxify({'url': 'http://uniqkid.us3.list-manage.com/subscribe/post?u=f4f4089d6992b16f24b9e6640&amp;id=8d386d8bbc'});
 
-    $('#signup-form').chimpaxify({'url': 'http://uniqkid.us3.list-manage.com/subscribe/post?u=f4f4089d6992b16f24b9e6640&amp;id=8d386d8bbc'});
-    console.log('sent to mail chimp');
-  });
 });

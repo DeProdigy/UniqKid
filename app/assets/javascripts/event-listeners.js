9923901bd415ca52029f80bf8161c41feec2cf5a
DeProdigy <alex.hint.ah@gmail.com>
2014-03-11 18:40:42 -0400

    fix mail chimp problem

--- app/assets/javascripts/event-listeners.js
+++ app/assets/javascripts/event-listeners.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   console.log( "Loaded Main Page" );
 
-  $('#signup-form').chimpaxify({'url': 'http://uniqkid.us3.list-manage.com/subscribe/post?u=f4f4089d6992b16f24b9e6640&amp;id=8d386d8bbc'});
+  $('#signup-form').chimpaxify({'url': 'http://uniqkid.us3.list-manage.com/subscribe/post?u=f4f4089d6992b16f24b9e6640&amp;id=8d386d8bbc'})
+                   .on('chimpaxify:callback', function(){
+                      this.reset();
+                   });
 
 });
